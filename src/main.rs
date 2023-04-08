cac58f53fdccb443da537e7f7044e161da6f6b84
vsensu <vsensu@foxmail.com>
2023-04-09 00:58:47 +0800

    add a simple scene and fps camera controll

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,309 @@
+//! This example demonstrates the built-in 3d shapes in Bevy.
+//! The scene includes a patterned texture and a rotation for visualizing the normals and UVs.
+
+use std::f32::consts::PI;
+
+use bevy::{
+    prelude::*,
+    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
+    window::PrimaryWindow,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_startup_system(setup)
+        .add_system(rotate)
+        .add_system(camera_movement)
+        .add_system(mouse_look)
+        .run();
+}
+
+/// A marker component for our shapes so we can query them separately from the ground plane
+#[derive(Component)]
+struct Shape;
+
+const X_EXTENT: f32 = 14.5;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut images: ResMut<Assets<Image>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let debug_material = materials.add(StandardMaterial {
+        base_color_texture: Some(images.add(uv_debug_texture())),
+        ..default()
+    });
+
+    let shapes = [
+        meshes.add(shape::Cube::default().into()),
+        meshes.add(shape::Box::default().into()),
+        meshes.add(shape::Capsule::default().into()),
+        meshes.add(shape::Torus::default().into()),
+        meshes.add(shape::Cylinder::default().into()),
+        meshes.add(shape::Icosphere::default().try_into().unwrap()),
+        meshes.add(shape::UVSphere::default().into()),
+    ];
+
+    let num_shapes = shapes.len();
+
+    for (i, shape) in shapes.into_iter().enumerate() {
+        commands.spawn((
+            PbrBundle {
+                mesh: shape,
+                material: debug_material.clone(),
+                transform: Transform::from_xyz(
+                    -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
+                    2.0,
+                    0.0,
+                )
+                .with_rotation(Quat::from_rotation_x(-PI / 4.)),
+                ..default()
+            },
+            Shape,
+        ));
+    }
+
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 9000.0,
+            range: 100.,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(8.0, 16.0, 8.0),
+        ..default()
+    });
+
+    // ground plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(50.0).into()),
+        material: materials.add(Color::SILVER.into()),
+        ..default()
+    });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 6., 12.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+        ..default()
+    });
+
+    commands.insert_resource(MyCamera {
+        speed: 10.0,
+        sensitivity: 0.2,
+        last_mouse_pos: None,
+    });
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_seconds() / 2.);
+    }
+}
+
+/// Creates a colorful test pattern
+fn uv_debug_texture() -> Image {
+    const TEXTURE_SIZE: usize = 8;
+
+    let mut palette: [u8; 32] = [
+        255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
+        198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
+    ];
+
+    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
+    for y in 0..TEXTURE_SIZE {
+        let offset = TEXTURE_SIZE * y * 4;
+        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
+        palette.rotate_right(4);
+    }
+
+    Image::new_fill(
+        Extent3d {
+            width: TEXTURE_SIZE as u32,
+            height: TEXTURE_SIZE as u32,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        &texture_data,
+        TextureFormat::Rgba8UnormSrgb,
+    )
+}
+
+#[derive(Resource, Default, Debug)]
+struct MyCamera {
+    speed: f32,
+    sensitivity: f32,
+    last_mouse_pos: Option<Vec2>,
+}
+
+// fn camera_movement(
+//     time: Res<Time>,
+//     keyboard_input: Res<Input<KeyCode>>,
+//     my_camera: Res<MyCamera>,
+//     mut query: Query<&mut Transform, With<Camera>>,
+// ) {
+//     let mut direction = Vec3::ZERO;
+//     if keyboard_input.pressed(KeyCode::W) {
+//         direction.y += 1.0;
+//     }
+//     if keyboard_input.pressed(KeyCode::A) {
+//         direction.x -= 1.0;
+//     }
+//     if keyboard_input.pressed(KeyCode::S) {
+//         direction.y -= 1.0;
+//     }
+//     if keyboard_input.pressed(KeyCode::D) {
+//         direction.x += 1.0;
+//     }
+//     if direction.length() > 0.0 {
+//         direction = direction.normalize();
+//     }
+
+//     let delta_seconds = time.delta_seconds();
+//     let translation = direction * my_camera.speed * delta_seconds;
+//     for mut transform in query.iter_mut() {
+//         let translation = transform.rotation.mul_vec3(translation);
+//         transform.translation += translation;
+//     }
+// }
+
+fn camera_movement(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Camera>>,
+    camera: Res<MyCamera>,
+) {
+    let mut direction = Vec3::ZERO;
+    if keyboard_input.pressed(KeyCode::W) {
+        direction.z -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        direction.x -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        direction.z += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        direction.x += 1.0;
+    }
+    if direction.length() > 0.0 {
+        direction = direction.normalize();
+    }
+    let delta_seconds = time.delta_seconds();
+    let translation = direction * camera.speed * delta_seconds;
+    let mut transform = query.single_mut();
+    let forward = transform.local_z();
+    let right = transform.local_x();
+    transform.translation += translation.x * right + translation.z * forward;
+}
+
+// fn mouse_look(
+//     time: Res<Time>,
+//     mut commands: Commands,
+//     mut primary_query: Query<&mut Window, With<PrimaryWindow>>,
+//     mut query: Query<&mut Transform, With<Camera>>,
+// ) {
+//     let Ok(mut primary) = primary_query.get_single_mut() else {
+//         return;
+//     };
+
+//     let delta = time.delta_seconds();
+//     let sensitivity = 0.2;
+//     let size = Vec2 {
+//         x: primary.width(),
+//         y: primary.height(),
+//     };
+//     let center = size / 2.0;
+//     let mut delta_mouse = primary.cursor_position().unwrap() - center;
+//     if delta_mouse.length_squared() > 0.0 {
+//         delta_mouse *= sensitivity * delta;
+//         let mut transform = query.single_mut();
+//         let yaw = Quat::from_rotation_y(-delta_mouse.x);
+//         let pitch = Quat::from_rotation_x(-delta_mouse.y);
+//         let rotation = yaw * pitch;
+//         transform.rotation = rotation * transform.rotation;
+//         primary.set_cursor_position(Some(center));
+//     }
+// }
+
+// fn mouse_look(
+//     time: Res<Time>,
+//     mut commands: Commands,
+//     mut primary: Query<&mut Window, With<PrimaryWindow>>,
+//     mut query: Query<&mut Transform, With<Camera>>,
+//     camera: Res<MyCamera>,
+// ) {
+//     let Ok(mut window) = primary.get_single_mut() else {
+//         return;
+//     };
+//     let delta = time.delta_seconds();
+//     let sensitivity = camera.sensitivity;
+//     let size = Vec2 {
+//         x: window.width(),
+//         y: window.height(),
+//     }; // Replace with your window size
+//     let center = size / 2.0;
+//     // let window = windows.get_primary_mut().unwrap();
+//     let mouse_pos = window.cursor_position().unwrap_or(center);
+
+//     let delta_mouse = match camera.last_mouse_pos {
+//         Some(last_pos) => mouse_pos - last_pos,
+//         None => Vec2::ZERO,
+//     };
+//     // camera.last_mouse_pos = Some(mouse_pos);
+//     commands.insert_resource(MyCamera {
+//         speed: camera.speed,
+//         sensitivity: camera.sensitivity,
+//         last_mouse_pos: Some(mouse_pos),
+//     });
+
+//     if delta_mouse.length_squared() > 0.0 {
+//         let mut transform = query.single_mut();
+//         let yaw = Quat::from_rotation_y(-delta_mouse.x * sensitivity);
+//         let pitch_angle = -delta_mouse.y * sensitivity;
+//         let pitch = Quat::from_rotation_x(pitch_angle);
+//         let rotation = yaw * pitch;
+//         transform.rotate(rotation);
+//         let y_rotation = transform.rotation.to_euler(EulerRot::YXZ).1;
+//         let limit = std::f32::consts::FRAC_PI_2 - 0.01;
+//         if y_rotation < -limit {
+//             transform.rotation = Quat::from_rotation_y(-limit) * Quat::from_rotation_x(pitch_angle);
+//         } else if y_rotation > limit {
+//             transform.rotation = Quat::from_rotation_y(limit) * Quat::from_rotation_x(pitch_angle);
+//         }
+//         window.set_cursor_position(center.into());
+//     }
+// }
+
+// first person camera
+fn mouse_look(
+    time: Res<Time>,
+    mut primary_query: Query<&mut Window, With<PrimaryWindow>>,
+    mut query: Query<&mut Transform, With<Camera>>,
+) {
+    let Ok(mut primary) = primary_query.get_single_mut() else {
+        return;
+    };
+
+    let delta = time.delta_seconds();
+    let sensitivity = 0.02;
+    let size = Vec2 {
+        x: primary.width(),
+        y: primary.height(),
+    };
+    let center = size / 2.0;
+
+    let mut delta_mouse = primary.cursor_position().unwrap_or(center) - center;
+    if delta_mouse.length_squared() > 0.0 {
+        let window_scale = primary.height().min(primary.width());
+        delta_mouse *= sensitivity * window_scale * delta;
+        let mut transform = query.single_mut();
+        let (mut yaw, mut pitch, _) = transform.rotation.to_euler(EulerRot::YXZ);
+        pitch -= -delta_mouse.y.to_radians();
+        yaw -= delta_mouse.x.to_radians();
+        pitch = pitch.clamp(-1.54, 1.54);
+        transform.rotation =
+            Quat::from_axis_angle(Vec3::Y, yaw) * Quat::from_axis_angle(Vec3::X, pitch);
+        primary.set_cursor_position(Some(center));
+    }
 }
