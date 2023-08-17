cb7ace7ca90868d57ab2ae189f29a374005918e3
vsensu <vsensu@foxmail.com>
2023-08-17 17:12:12 +0800

    wireframe & fps

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ mod voxel;
 use std::f32::consts::PI;
 
 use bevy::{
+    diagnostic::{Diagnostics, DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+    pbr::wireframe::{Wireframe, WireframeConfig, WireframePlugin},
     prelude::*,
     render::render_resource::{Extent3d, TextureDimension, TextureFormat},
     window::PrimaryWindow,
@@ -11,6 +13,7 @@ use smooth_bevy_cameras::{
     controllers::fps::{FpsCameraBundle, FpsCameraController, FpsCameraPlugin},
     LookTransformPlugin,
 };
+
 use voxel::ChunkIndex;
 
 /// A marker component for our shapes so we can query them separately from the ground plane
@@ -19,6 +22,7 @@ pub struct Shape;
 
 pub fn setup(
     mut commands: Commands,
+    asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
@@ -67,6 +71,35 @@ pub fn setup(
         sensitivity: 0.02,
         ui_mode: true,
     });
+
+    let text_section = move |color, value: &str| {
+        TextSection::new(
+            value,
+            TextStyle {
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                font_size: 20.0,
+                color,
+            },
+        )
+    };
+
+    commands.spawn((
+        TextBundle::from_sections([
+            text_section(Color::GREEN, "FPS (raw): "),
+            text_section(Color::CYAN, ""),
+            text_section(Color::GREEN, "\nFPS (SMA): "),
+            text_section(Color::CYAN, ""),
+            text_section(Color::GREEN, "\nFPS (EMA): "),
+            text_section(Color::CYAN, ""),
+        ])
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            top: Val::Px(5.0),
+            left: Val::Px(5.0),
+            ..default()
+        }),
+        StatsText,
+    ));
 }
 
 pub fn post_setup(ms: Res<MouseSettings>, mut fps_camera_query: Query<&mut FpsCameraController>) {
@@ -108,4 +141,40 @@ pub fn input_mode(
         let center = size / 2.0;
         primary.set_cursor_position(Some(center));
     }
+}
+
+use bevy_inspector_egui::prelude::*;
+use bevy_inspector_egui::quick::ResourceInspectorPlugin;
+
+// `InspectorOptions` are completely optional
+#[derive(Reflect, Resource, Default, InspectorOptions)]
+#[reflect(Resource, InspectorOptions)]
+pub struct DebugSettings {
+    wireframe: bool,
+}
+
+pub fn debug_system(
+    debug_settings: Res<DebugSettings>,
+    mut wireframe_config: ResMut<WireframeConfig>,
+) {
+    wireframe_config.global = debug_settings.wireframe;
+}
+
+#[derive(Component)]
+pub struct StatsText;
+
+pub fn fps(diagnostics: Res<DiagnosticsStore>, mut query: Query<&mut Text, With<StatsText>>) {
+    let mut text = query.single_mut();
+
+    if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+        if let Some(raw) = fps.value() {
+            text.sections[1].value = format!("{raw:.2}");
+        }
+        if let Some(sma) = fps.average() {
+            text.sections[3].value = format!("{sma:.2}");
+        }
+        if let Some(ema) = fps.smoothed() {
+            text.sections[5].value = format!("{ema:.2}");
+        }
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use bevy::diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::pbr::wireframe::{Wireframe, WireframeConfig, WireframePlugin};
 use bevy::prelude::*;
+use bevy::window::PresentMode;
+use bevy_inspector_egui::prelude::*;
+use bevy_inspector_egui::quick::ResourceInspectorPlugin;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use smooth_bevy_cameras::{
     controllers::fps::{FpsCameraBundle, FpsCameraController, FpsCameraPlugin},
@@ -7,23 +12,32 @@ use smooth_bevy_cameras::{
 
 fn main() {
     App::new()
-        .add_plugins(
+        .add_plugins((
             DefaultPlugins
                 .set(ImagePlugin::default_nearest())
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         fit_canvas_to_parent: true,
+                        present_mode: PresentMode::AutoNoVsync,
                         ..default()
                     }),
                     ..default()
                 }),
-        )
+            WireframePlugin,
+        ))
         .add_plugins(WorldInspectorPlugin::new())
         .add_plugins(LookTransformPlugin)
         .add_plugins(FpsCameraPlugin::default())
+        .add_plugins(FrameTimeDiagnosticsPlugin::default())
+        // .add_plugins(EguiPlugin)
         .add_systems(Startup, mcrs::setup)
         .add_systems(PostStartup, mcrs::post_setup)
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Update, mcrs::input_mode)
+        .init_resource::<mcrs::DebugSettings>() // `ResourceInspectorPlugin` won't initialize the resource
+        .register_type::<mcrs::DebugSettings>() // you need to register your type to display it
+        .add_plugins(ResourceInspectorPlugin::<mcrs::DebugSettings>::default())
+        .add_systems(Update, mcrs::debug_system)
+        .add_systems(Update, mcrs::fps)
         .run();
 }
